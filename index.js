51e10f13272accab0e8bfe718ae4e691fc0942e7
李祥彪 <xiangbiao.li@ur.com.cn>
2023-06-05 17:47:34 +0800

    feat: mount static

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const path = require('path');
 
 const RedisUtils = require('./utils/redis');
 const useRouter = require('./router');
